757a8526b450c3f327195c89c5d5b420e7bd4d4b
Joel Ibaceta <niohnex@gmail.com>
2016-06-30 03:41:16 -0300

    debug added

--- pulpobot/commands/questions.rb
+++ pulpobot/commands/questions.rb
@@ -1,9 +1,10 @@
 module PulpoBot
   module Commands
     class Questions < SlackRubyBot::Commands::Base
-      bot = Cleverbot.new(ENV['API_USER'], ENV['API_KEY'], 'sessionname')
+      bot = Cleverbot.new('1d4seUHqQlPX1iTE', 'pU2ZVB0g0cRkGiFIbxVcSdczvcW87V3K', 'sessionname2')
   
       match(/^(?<bot>\w*)\s(?<expression>.*)$/) do |client, data, match| 
+        puts "CLIENT: #{client}   |   DATA: #{data}   |   MATCH: #{match} "
         client.say(channel: data.channel, text: bot.say(match[:expression]))
       end
     end
