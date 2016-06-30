260b176858d5a2ed7805fb97a53f2e42b789337d
Joel Ibaceta <niohnex@gmail.com>
2016-06-30 18:05:41 -0300

    one command

--- pulpobot/commands/questions.rb
+++ pulpobot/commands/questions.rb
@@ -4,7 +4,8 @@ module PulpoBot
       
       bot = Cleverbot.new('1d4seUHqQlPX1iTE', 'pU2ZVB0g0cRkGiFIbxVcSdczvcW87V3K')
   
-      match(/^(?<bot>\w*)\s(?<expression>.*)$/) do |client, data, match| 
+      match(/^@(?<bot>\w*):\s(?<expression>.*)$/) do |client, data, match| 
+        
         puts "CLIENT: #{client}   |   DATA: #{data}   |   MATCH: #{match[:expression]} " 
         client.say(channel: data.channel, text: bot.say(match[:expression]))
       end
