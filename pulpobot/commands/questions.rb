84c4021c6e88684243fc15c265906699f6554737
Joel Ibaceta <niohnex@gmail.com>
2016-06-30 04:40:24 -0300

    session id removed

--- pulpobot/commands/questions.rb
+++ pulpobot/commands/questions.rb
@@ -1,7 +1,7 @@
 module PulpoBot
   module Commands
     class Questions < SlackRubyBot::Commands::Base
-      bot = Cleverbot.new('1d4seUHqQlPX1iTE', 'pU2ZVB0g0cRkGiFIbxVcSdczvcW87V3K', Time.now.to_s)
+      bot = Cleverbot.new('1d4seUHqQlPX1iTE', 'pU2ZVB0g0cRkGiFIbxVcSdczvcW87V3K')
   
       match(/^(?<bot>\w*)\s(?<expression>.*)$/) do |client, data, match| 
         puts "CLIENT: #{client}   |   DATA: #{data}   |   MATCH: #{match[:expression]} " 
