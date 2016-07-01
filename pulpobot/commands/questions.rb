b52c737ab6655356f48302a58a2611257b2d46dc
Joel Ibaceta <niohnex@gmail.com>
2016-07-01 14:19:35 -0300

    match

--- pulpobot/commands/questions.rb
+++ pulpobot/commands/questions.rb
@@ -3,6 +3,8 @@ module PulpoBot
     class Questions < SlackRubyBot::Commands::Base
       
       bot = Cleverbot.new('1d4seUHqQlPX1iTE', 'pU2ZVB0g0cRkGiFIbxVcSdczvcW87V3K')
+      
+      @mp_account = ""
   
       match(/^(?<bot>\w*)\s(?<expression>.*)$/) do |client, data, match| 
         
@@ -11,21 +13,30 @@ module PulpoBot
         expression = match[:expression] 
         
         mp_match_money_request = /cobrale (?<amount>.*) a (?<person>.*)$/.match(expression)
+        mp_account_request = /^mi email de mercadopago es (?<account>.*)$/.match(expression)
         
         
         p "MATCH MP: #{mp_match_money_request.inspect} \n"
         
-        if mp_match_money_request != nil 
+        if mp_account_request != nil
+          
+          @mp_account = mp_account_request[:account]
+        
+        elsif mp_match_money_request != nil 
           
           money_request       = MercadoPago::MoneyRequest.new
           money_request.currency_id   = "ARS"
           money_request.amount        = mp_match_money_request[:amount].scan(/\d/).join('').to_f
-          money_request.payer_email   = "niohnex@gmail.com"
+          money_request.payer_email   = @mp_account
           money_request.description   = "PulpoBot Request"
           money_request.concept_type  = "off_platform"
-          money_request.save
-          
-          client.say(channel: data.channel, text: "#{mp_match_money_request[:person]} pagale los #{mp_match_money_request[:amount]} aqui: #{money_request.init_point}")
+          begin
+            money_request.save
+            client.say(channel: data.channel, text: "#{mp_match_money_request[:person]} pagale los #{mp_match_money_request[:amount]} aqui: #{money_request.init_point}")
+          rescue Exception => e
+            client.say(channel: data.channel, text: e.message
+          end
+           
           
         else
           client.say(channel: data.channel, text: bot.say(match[:expression]))
