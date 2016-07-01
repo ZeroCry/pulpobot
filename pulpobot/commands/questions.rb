f1334fab309dc8e37e1457d333e3b6b32fcf7765
Joel Ibaceta <niohnex@gmail.com>
2016-07-01 14:04:55 -0300

    command

--- pulpobot/commands/questions.rb
+++ pulpobot/commands/questions.rb
@@ -19,13 +19,13 @@ module PulpoBot
           
           money_request       = MercadoPago::MoneyRequest.new
           money_request.currency_id   = "ARS"
-          money_request.amount        = mp_match[:amount].scan(/\d/).join('').to_f
+          money_request.amount        = mp_match_money_request[:amount].scan(/\d/).join('').to_f
           money_request.payer_email   = "niohnex@gmail.com"
           money_request.description   = "PulpoBot Request"
           money_request.concept_type  = "off_platform"
           money_request.save
           
-          client.say(channel: data.channel, text: "#{mp_match[:person]} pagale los #{mp_match[:amount]} aqui: #{money_request.init_point}")
+          client.say(channel: data.channel, text: "#{mp_match_money_request[:person]} pagale los #{mp_match_money_request[:amount]} aqui: #{money_request.init_point}")
           
         else
           client.say(channel: data.channel, text: bot.say(match[:expression]))
