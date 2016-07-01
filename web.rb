c0e0062655aa32960ae2dc072e2c27c9f7dc4c43
Joel Ibaceta <niohnex@gmail.com>
2016-07-01 16:09:27 -0300

    web

--- web.rb
+++ web.rb
@@ -1,8 +1,10 @@
 require 'sinatra/base'
+require 'mercadopago'
 
 module PulpoBot
   
   MercadoPago::Settings.ACCESS_TOKEN  = "TEST-6295877106812064-042916-6cead5bc1e48af95ea61cc9254595865__LC_LA__-202809963"
+  
   
   class Web < Sinatra::Base
     get '/' do
