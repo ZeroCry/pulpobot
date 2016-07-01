fb5cdad75bd4d74c34570c8b86d0bdaaf154fc36
Joel Ibaceta <niohnex@gmail.com>
2016-07-01 16:12:44 -0300

    message improved

--- pulpobot/commands/questions.rb
+++ pulpobot/commands/questions.rb
@@ -40,7 +40,7 @@ module PulpoBot
               if response.code.to_s == "200" 
                 client.say(channel: data.channel, text: "#{mp_match_money_request[:person]} pagale los #{mp_match_money_request[:amount]} aqui: #{money_request.init_point}")
               else 
-                client.say(channel: data.channel, text: response.message)
+                client.say(channel: data.channel, text: %W(#{response.message} : #{response.body["response.message"]}))
               end
             end
           rescue Exception => e
