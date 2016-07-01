ef1903b796d0edb7a0be7b02a58c84c0a0f471a5
Joel Ibaceta <niohnex@gmail.com>
2016-07-01 14:58:29 -0300

    credentilas updated

--- web.rb
+++ web.rb
@@ -2,7 +2,7 @@ require 'sinatra/base'
 
 module PulpoBot
   
-  MercadoPago::Settings.ACCESS_TOKEN  = "TEST-3964826791704277-042213-a8239945016c46f25a87aafebd74bab3__LD_LB__-202809963"
+  MercadoPago::Settings.ACCESS_TOKEN  = "TEST-6295877106812064-042916-6cead5bc1e48af95ea61cc9254595865__LC_LA__-202809963"
   
   class Web < Sinatra::Base
     get '/' do
